92c5d8d724481c3146d5965e782c0cb2473bdb8c
Travis Hathaway <travis.j.hathaway@gmail.com>
2015-11-15 01:47:29 +0100

    we now have a working, but very busy graph. Next step is to add filters

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -12,10 +12,22 @@ angular.module('crimestatsApp').controller(
     '$scope', '$http',
     function ($scope, $http) {
       $http.get('/api/crime_stats').success(function(data){
+        var rows = [];
+        for(var x = 0; x < data.results.rows.length; x++){
+            var row;
+            row = data.results.rows[x];
+            row.c[0].v = new Date(row.c[0].v);
+            rows.push(row);
+        }
+
         var chart1 = {};
         chart1.type = "LineChart";
-        chart1.data = data.results;
-        chart1.cssStyle = "height:200px; width:300px;";
+        chart1.data = {
+            cols: data.results.cols,
+            rows: rows
+        };
+
+        chart1.cssStyle = "height:500px; width: 100%";
         chart1.options = {
             "title": "Crime Incidents per month",
             "fill": 20,
