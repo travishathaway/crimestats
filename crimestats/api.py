786f84cba9e2e37894b2b4d2c60d47746b244e78
Travis Hathaway <travis.j.hathaway@gmail.com>
2015-11-15 04:03:56 +0100

    removing some cruft and adding more speed to our API call

--- crimestats/api.py
+++ crimestats/api.py
@@ -1,38 +1,63 @@
-from sqlalchemy import *
+from sqlalchemy.dialects import postgresql
 from flask import g
 from flask_restful import Resource
 from webargs import fields
 from webargs.flaskparser import use_args 
 import pandas as pd
 
-MAJOR_OFFENSES_BY_YEAR = """
-"""
 
-MAJOR_OFFENSES_BY_YEAR_MONTH = """
-SELECT * FROM major_offense_type_by_year_month;
-"""
+def format_for_google_chart(df):
+    """
+    Formats a Pandas data frame for use in Angular Google
+    Charts.
 
+    :param df: pandas.Dataframe
+    :returns: Dictionary
+    """
+    data = df.to_dict(orient='split')
 
-crime_stats_args = {
-    'major_offense_type': fields.List(fields.Str())
-}
+    results = {}
+    results['cols'] = []
+    results['rows'] = []
+
+    for col in data['columns']:
+        if col == 'date':
+            results['cols'].append({
+                "id": col,
+                "type": "date",
+                "label": col
+            })
+        else:
+            results['cols'].append({
+                "id": col,
+                "type": "number",
+                "label": col
+            })
+
+    for row in data['data']:
+        r = []
+        for val in row:
+            r.append({
+                "v": val
+            })
+        results['rows'].append({
+            "c": r
+        })
+
+    return results
 
 
 class CrimeStatsAPI(Resource):
+    crime_stats_args = {
+        'major_offense_type': fields.List(fields.Str())
+    }
+
     @use_args(crime_stats_args)
     def get(self, args):
         """
 
         :return:
         """
-
-        # major_offense_by_year_month = Table(
-        #     "major_offense_type_by_year_month", g.db.metadata,
-        #     Column("date", Date),
-        #     Column("major_offense_type", Unicode(23)),
-        #     Column("count", Integer),
-        #     autoload=True, autoload_with=g.db.engine
-        # )
         ses = g.db.session
 
         query = ses.query(g.CrimeStatsYearMonth)
@@ -44,44 +69,24 @@ class CrimeStatsAPI(Resource):
                 )
             )
 
-        results = query.all()
-        res = [r.to_dict() for r in results]
-
-        df = pd.DataFrame(res)
+        # Let pandas execute the SQL statement to load it straight in to
+        # the Dataframe.
+        df = pd.read_sql_query(query.selectable, g.db.engine)
+        df.rename(columns={
+            'major_offense_type_by_year_month_date': 'date',
+            'major_offense_type_by_year_month_count': 'count',
+            'major_offense_type_by_year_month_major_offense_type': 'major_offense_type'
+        }, inplace=True)
+        # datetime.date objects are not JSON serializable
+        df['date'] = df['date'].apply(lambda x: str(x))
 
         df = df.pivot_table(
             index=['date'], columns=['major_offense_type']
         ).fillna(0)['count']
  
-        data = df.reset_index(level=0).to_dict(orient='split')
-    
-        results = {}
-        results['cols'] = []
-        results['rows'] = []
+        df = df.reset_index(level=0)
 
-        for col in data['columns']:
-            if col == 'date':
-                results['cols'].append({
-                    "id": col,
-                    "type": "date",
-                    "label": col
-                })
-            else:
-                results['cols'].append({
-                    "id": col,
-                    "type": "number",
-                    "label": col
-                })
-
-        for row in data['data']:
-            r = []
-            for val in row:
-                r.append({
-                    "v": val
-                })
-            results['rows'].append({
-                "c": r
-            })
+        results = format_for_google_chart(df)
 
         return {
             'results': results 
